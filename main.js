d913ac624533e044df3eba7e6ee4b9920c6eabfd
Waldemar Adams <waldek-adamski@wp.pl>
2021-02-18 16:48:40 +0100

    add basic style in css

--- main.js
+++ main.js
@@ -2,36 +2,36 @@ const generator = document.querySelector('.generator');
 const button = document.querySelector('.BtnInput');
 
 const getUsers = (e) => {
-    e.preventDefault();
-    const userNumber = document.querySelector('.generator__number').value;
-    const userGender = document.querySelector('.generator__select').value;
+  e.preventDefault();
+  const userNumber = document.querySelector('.generator__number').value;
+  const userGender = document.querySelector('.generator__select').value;
 
-    // console.log(userGender, userNumber);
-    // console.log('okok');
-    const url = `https://randomuser.me/api/?results=${userNumber}&gender=${userGender}`;
-    console.log(url);
-    fetch(url)
-        .then((response) => {
-            return response.json()
+  // console.log(userGender, userNumber);
+  // console.log('okok');
+  const url = `https://randomuser.me/api/?results=${userNumber}&gender=${userGender}`;
+  console.log(url);
+  fetch(url)
+    .then((response) => {
+      return response.json()
 
-        })
-        .then(json => showUsers(json.results))
+    })
+    .then(json => showUsers(json.results))
 }
 
 const showUsers = (usersArray) => {
-    console.log(usersArray);
-    const divShowResults = document.querySelector('.usersResults');
-    usersArray.forEach((user) => {
-        const divUser = document.createElement('div');
-        divUser.className = 'user';
-        divUser.innerHTML = `
-      <div class='divUser__name'>
+  console.log(usersArray);
+  const divShowResults = document.querySelector('.usersResults');
+  usersArray.forEach((user) => {
+    const divUser = document.createElement('div');
+    divUser.className = 'item';
+    divUser.innerHTML = `
+      <div class='item__name'>
     ${user.name.title.toUpperCase()} ${user.name.first.toUpperCase()} ${user.name.last.toUpperCase()}
     </div>
-    <img class='divUser__img' src=${user.picture.medium}>
+    <img class='item__img' src=${user.picture.medium}>
       `
-        divShowResults.appendChild(divUser)
-    })
+    divShowResults.appendChild(divUser)
+  })
 
 }
 
